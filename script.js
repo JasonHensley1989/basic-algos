47560555b65699c9745507b1afb5f1fa3811ad10
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-20 10:31:05 -0600

    recursion testing

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-function multiplyAll(arr) {
-    let product = 1;
-    // Only change code below this line
-  for(let i = 0; i < arr.length; i++) {
-    for(let j = 0; j < arr[i].length; j++) {
-      product = product * arr[i][j];
-      console.log(product)
-    }
-  }
-    // Only change code above this line
-    console.log(product)
-    return product;
-  }
+// function multiplyAll(arr) {
+//     let product = 1;
+//     // Only change code below this line
+//   for(let i = 0; i < arr.length; i++) {
+//     for(let j = 0; j < arr[i].length; j++) {
+//       product = product * arr[i][j];
+//       console.log(product)
+//     }
+//   }
+//     // Only change code above this line
+//     console.log(product)
+//     return product;
+//   }
   
-  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+//   multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+//   const myArray = [];
+// let i = 10;
+// // while (i < 5) {
+// //   myArray.push(i);
+// //   i++;
+// // }
+
+// do {
+//   myArray.push(i);
+//   i++;
+// } while (i < 5)
+
+// console.log(myArray);
